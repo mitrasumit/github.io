c203965cadcf5a7f0a87a2445cfb65df1b16dab6
Sumit Mitra <mitra.sumit@outlook.com>
2020-07-31 06:24:28 +0530

    Update scripts.js
    Added Gear

--- scripts.js
+++ scripts.js
@@ -3,4 +3,12 @@ function backtotop()
     document.body.scrollTop=0;
     document.documentElement.scrollTop = 0;
 }
+window.onscroll=function()
+{
+    scrollRotate();
+};
 
+function scrollRotate() {
+    let image = document.getElementById("navgear");
+    image.style.transform = "rotate(" + window.pageYOffset*2 + "deg)";
+}
